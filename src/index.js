622713f59156b0f05d1ae6f4f81eefb0bceba2df
Laura Pham <l.pham2311@gmail.com>
2020-05-13 19:41:10 +1000

    Added back App.js and Navbar

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-// import App from "./App";
+import App from "./App";
 // import registerServiceWorker from ".registerServiceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import Counters from "./components/counters";
+// import Counters from "./components/counters";
 
 // const element = <h1>Hello World</h1>;
 // Index will render App Component
-ReactDOM.render(<Counters />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
